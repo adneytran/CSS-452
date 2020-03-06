eaa6aaa41af708af97181885822611820a330908
adneytran <37353282+adneytran@users.noreply.github.com>
2020-03-06 03:59:04 -0800

    unkellaned_code

---
+++ AndyTran_MP5/public_html/src/MyGame/Enemy/PatrolHead.js
@@ -0,0 +1,23 @@
+/*jslint node: true, vars: true */
+/*global gEngine, Scene, GameObjectset, TextureObject, Camera, vec2,
+  FontRenderable, SpriteRenderable, LineRenderable,
+  GameObject */
+
+function PatrolHead() {
+    this.headSprite = null;
+    this.kSpriteSheet = "assets/SpriteSheet.png";
+    gEngine.Textures.loadTexture(this.kSpriteSheet);
+}
+
+PatrolHead.prototype.initialize = function (aStartingPoint) {
+    this.headSprite = new SpriteRenderable(this.kSpriteSheet);
+    this.headSprite.getXform().setPosition(aStartingPoint[0], aStartingPoint[1]);
+    this.headSprite.getXform().setSize(7.5, 7.5);
+    var texWidth = this.headSprite.mTexWidth;
+    var texHeight = this.headSprite.mTexHeight;
+    this.headSprite.setElementUVCoordinate(130 / texWidth, 310 / texWidth, 0 / texHeight, 180 / texHeight);
+    var patrolColor = [1, 1, 1, 0];
+    this.headSprite.setColor(patrolColor);
+};
+
+
